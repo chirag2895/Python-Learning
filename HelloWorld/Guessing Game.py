9218945317610c1c54e718f11008c5bb05457918
Chirag Patel <chirag2895@hotmail.com>
2020-06-30 14:21:43 -0400

    Add files via upload

---
+++ HelloWorld/Guessing Game.py
@@ -0,0 +1,44 @@
+answer = 5
+
+print("Please guess a number between 1 and 10: ")
+guess = int(input())
+
+# if guess < answer:
+#     print("Please guess higher")
+# elif guess > answer:
+#     print("Please guess lower")
+# else:
+#     print("You got it first time")
+
+# Adding second guess
+# if guess < answer:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guess correctly")
+# else:
+#     print("You got it first time")
+
+
+# Same but without using elif
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
+else:
+    print("You got it first time")
